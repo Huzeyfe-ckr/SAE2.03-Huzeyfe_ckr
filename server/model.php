afc3792da97ec5ace262b67608357d969f83c471
Huzeyfe Cakir <huzeyfe.cakir@etu.unilim.fr>
2025-04-03 10:40:00 +0200

    maj credenntials bdd

--- server/model.php
+++ server/model.php
@@ -14,6 +14,6 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "mora");
-define("DBLOGIN", "root");
-define("DBPWD", "root");
+define("DBNAME", "cakir4");
+define("DBLOGIN", "cakir4");
+define("DBPWD", "cakir4");
