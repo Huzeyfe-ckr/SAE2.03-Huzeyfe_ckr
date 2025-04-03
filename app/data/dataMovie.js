65a19171203bc9fae77a916e745b537a9fe8c23d
Huzeyfe Cakir <huzeyfe.cakir@etu.unilim.fr>
2025-04-03 11:55:39 +0200

    film

--- app/data/dataMovie.js
+++ app/data/dataMovie.js
@@ -2,24 +2,14 @@
 // URL où se trouve le répertoire "server" sur mmi.unilim.fr
 let HOST_URL = "..";//"http://mmi.unilim.fr/~????"; // CHANGE THIS TO MATCH YOUR CONFIG
 
-let DataFilm = {};
+let DataMovie = {};
 
- /**
-     * Fetches data from the server based on the specified day.
-     *
-     * @param {string} week - The week parameter to be sent to the server.
-     * @param {string} day - The day parameter to be sent to the server.
-     * @returns The response from the server.
-     * 
-     * DataFilm.request permet de récupérer des données depuis le serveur.
-     * Elle prend en paramètre une semaine (1, 2, ..., 52) et un jour (lundi mardi...)
-     * renvoie les données contenues dans la réponse du serveur (data).
-     */
-DataFilm.request = async function(week, day){
+
+DataMovie.request = async function(){
     // fetch permet d'envoyer une requête HTTP à l'URL spécifiée. 
     // L'URL est construite en concaténant HOST_URL à "/server/script.php?direction=" et la valeur de la variable dir. 
     // L'URL finale dépend de la valeur de HOST_URL et de dir.
-    let answer = await fetch(HOST_URL + "/server/script.php?todo=read&semaine=" + week + "&jour=" + day);
+    let answer = await fetch(HOST_URL + "/server/script.php?todo=readmovies=" );
     // answer est la réponse du serveur à la requête fetch.
     // On utilise ensuite la méthode json() pour extraire de cette réponse les données au format JSON.
     // Ces données (data) sont automatiquement converties en objet JavaScript.
@@ -40,4 +30,6 @@ DataFilm.request = async function(week, day){
 
 */
 
-export {DataFilm};
+export {DataMovie
+
+};
